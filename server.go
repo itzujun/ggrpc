8a27c0ec5a434ecc1dabb612a0886d0f51d4280f
open_china <953761580@qq.com>
2019-03-20 16:09:44 +0800

    局域网调用

--- server.go
+++ server.go
@@ -8,11 +8,38 @@ import (
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"os"
+	"strings"
 )
 
 const (
 	port = "127.0.0.1:50051"
 )
+
+
+//局域网设备访问需要绑局域网地址
+func getIP()string{
+	name, err := os.Hostname()
+	if err != nil {
+		fmt.Printf("Oops: %v\n", err)
+		return ""
+	}
+	addrs, err := net.LookupHost(name)
+	if err != nil {
+		fmt.Printf("Oops: %v\n", err)
+		return ""
+	}
+
+	for _, a := range addrs {
+		fmt.Println(a)
+		if strings.Contains(a,"192.168"){
+			return  a
+		}
+	}
+	return ""
+}
+
+
 
 type server struct{}
 
